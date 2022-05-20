b99a30dfc8be0d27be2a774677f87baaf41feb5d
JorgeRojas827 <jorgerojaspoma09@gmail.com>
2022-05-20 00:25:10 -0500

    first commit

---
+++ WCF-Colegio/IServicioAlumno.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Colegio
+{
+    // NOTA: puede usar el comando "Cambiar nombre" del menú "Refactorizar" para cambiar el nombre de interfaz "IServicioAlumno" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServicioAlumno
+    {
+        [OperationContract]
+        Boolean InsertAlumno(AlumnoBE objAlumnoBE);
+
+        [OperationContract]
+        Boolean UpdateAlumno(AlumnoBE objAlumnoBE);
+
+        [OperationContract]
+        Boolean DeleteAlumno(Int16 strId);
+
+        [OperationContract]
+        List<AlumnoBE> GetAllAlumnos();
+    }
+
+    [DataContract]
+    [Serializable]
+    public class AlumnoBE
+    {
+        // TODO
+        private short mvarid_alumno;
+        private Int16 mvarvalorcod_al;
+        private String mvarcod_al;
+        private String mvarnom_al;
+        private String mvarape_al;
+        private String mvardocide_al;
+        private DateTime mvarfecnac_al;
+        private String mvarsex_al;
+        private String mvarciu_al;
+        private String mvardir_al;
+        private Boolean mvaract_al;
+        private DateTime mvarfecres_al;
+
+        [DataMember]
+        public short Mvarid_alumno { get => mvarid_alumno; set => mvarid_alumno = value; }
+        [DataMember]
+        public short Mvarvalorcod_al { get => mvarvalorcod_al; set => mvarvalorcod_al = value; }
+        [DataMember] 
+        public string Mvarcod_al { get => mvarcod_al; set => mvarcod_al = value; }
+        [DataMember] 
+        public string Mvarnom_al { get => mvarnom_al; set => mvarnom_al = value; }
+        [DataMember] 
+        public string Mvarape_al { get => mvarape_al; set => mvarape_al = value; }
+        [DataMember] 
+        public string Mvardocide_al { get => mvardocide_al; set => mvardocide_al = value; }
+        [DataMember] 
+        public DateTime Mvarfecnac_al { get => mvarfecnac_al; set => mvarfecnac_al = value; }
+        [DataMember]
+        public string Mvarsex_al { get => mvarsex_al; set => mvarsex_al = value; }
+        [DataMember] 
+        public string Mvarciu_al { get => mvarciu_al; set => mvarciu_al = value; }
+        [DataMember] 
+        public string Mvardir_al { get => mvardir_al; set => mvardir_al = value; }
+        
+        [DataMember] 
+        public DateTime Mvarfecres_al { get => mvarfecres_al; set => mvarfecres_al = value; }
+    }
+}
---
+++ WCF-Colegio/ServicioAlumno.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Data.Entity.Core;
+
+namespace WCF_Colegio
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioAlumno" en el código y en el archivo de configuración a la vez.
+    public class ServicioAlumno : IServicioAlumno
+    {
+        public bool DeleteAlumno(Int16 strId)
+        {
+            BDCOLEGIOEntities bdcolegio = new BDCOLEGIOEntities();
+            try
+            {
+                ALUMNO objAlumno= (
+                    from oAlumno in bdcolegio.ALUMNO
+                    where oAlumno.IdAlumno == strId
+                    select oAlumno).FirstOrDefault();
+
+                bdcolegio.ALUMNO.Remove(objAlumno);
+                bdcolegio.SaveChanges();
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<AlumnoBE> GetAllAlumnos()
+        {
+            BDCOLEGIOEntities bdcolegio = new BDCOLEGIOEntities();
+            try
+            {
+                List<AlumnoBE> objListaAlumno = new List<AlumnoBE>();
+
+                var query = (from oAlumno in bdcolegio.ALUMNO select oAlumno);
+
+                foreach (var objAlumno in query)
+                {
+                    AlumnoBE objAlumnoBE = new AlumnoBE();
+
+                    objAlumnoBE.Mvarid_alumno = (short) objAlumno.IdAlumno;
+                    objAlumnoBE.Mvarnom_al = objAlumno.Nombres;
+                    objAlumnoBE.Mvarape_al = objAlumno.Apellidos;
+                    objAlumnoBE.Mvardocide_al = objAlumno.DocumentoIdentidad;
+                    objAlumnoBE.Mvarfecnac_al = Convert.ToDateTime(objAlumno.FechaNacimiento);
+                    objAlumnoBE.Mvarsex_al= objAlumno.Sexo;
+                    objAlumnoBE.Mvarciu_al = objAlumno.Ciudad;
+                    objAlumnoBE.Mvardir_al = objAlumno.Direccion;
+                    objAlumnoBE.Mvarciu_al = objAlumno.Ciudad;
+                    objAlumnoBE.Mvarfecres_al = Convert.ToDateTime(objAlumno.FechaNacimiento);
+
+                    objListaAlumno.Add(objAlumnoBE);
+                }
+
+                return objListaAlumno;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+
+        public bool InsertAlumno(AlumnoBE objAlumnoBE)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool UpdateAlumno(AlumnoBE objAlumnoBE)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
