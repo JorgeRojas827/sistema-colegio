1d0ce9fb268558157cdcf89e85ea0b8c290eea11
Carlos Quispe <carlosquispe1805@gmail.com>
2022-05-30 18:40:19 -0500

    CRUD de la tabla CURSO agregado

---
+++ WCF-Colegio/IServicioCurso.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Colegio
+{
+    // NOTA: puede usar el comando "Cambiar nombre" del menú "Refactorizar" para cambiar el nombre de interfaz "IServicioCurso" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IServicioCurso
+    {
+        [OperationContract]
+        Boolean InsertCurso(CursoBE objCursoBE);
+
+        [OperationContract]
+        Boolean UpdateCurso(CursoBE objCursoBE);
+
+        [OperationContract]
+        Boolean DeleteCurso(short strId);
+
+        [OperationContract]
+        List<CursoBE> GetCursos();
+    }
+
+    [DataContract]
+    [Serializable]
+    public class CursoBE
+    {
+        private short mvarid_curso;
+        private String mvardescripcion;
+        private Boolean mvaractivo;
+        private DateTime mvarfecharegistro;
+
+        [DataMember]
+        public short IdCurso
+        {
+            get { return mvarid_curso; }
+            set { mvarid_curso = value; }
+        }
+
+        [DataMember]
+        public String Descripcion
+        {
+            get { return mvardescripcion; }
+            set { mvardescripcion = value; }
+        }
+
+        [DataMember]
+        public DateTime FechaRegistro
+        {
+            get { return mvarfecharegistro; }
+            set { mvarfecharegistro = value; }
+        }
+    }
+}
---
+++ WCF-Colegio/ServicioCurso.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+using System.Data.Entity.Core;
+
+namespace WCF_Colegio
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de clase "ServicioCurso" en el código y en el archivo de configuración a la vez.
+    public class ServicioCurso : IServicioCurso
+    {
+        public Boolean InsertCurso(CursoBE objCursoBE)
+        {
+            BDCOLEGIOEntities bdcolegio = new BDCOLEGIOEntities();
+            try
+            {
+                CURSO objCurso = new CURSO();
+
+                objCursoBE.IdCurso = Int16.Parse(String.Empty);
+                objCursoBE.Descripcion = objCurso.Descripcion;
+
+                bdcolegio.CURSO.Add(objCurso);
+                bdcolegio.SaveChanges();
+
+                return true;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Boolean UpdateCurso(CursoBE objCursoBE)
+        {
+            BDCOLEGIOEntities bdcolegio = new BDCOLEGIOEntities();
+            try
+            {
+                CURSO objCurso = (
+                        from oCurso in bdcolegio.CURSO
+                        where oCurso.IdCurso == objCursoBE.IdCurso
+                        select oCurso
+                    ).FirstOrDefault();
+
+                objCurso.IdCurso = objCursoBE.IdCurso;
+                objCurso.Descripcion = objCursoBE.Descripcion;
+
+                bdcolegio.SaveChanges();
+
+                return true;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public Boolean DeleteCurso(short strId)
+        {
+            BDCOLEGIOEntities bdcolegio = new BDCOLEGIOEntities();
+
+            try
+            {
+                CURSO objCurso = (
+                        from oCurso in bdcolegio.CURSO
+                        where oCurso.IdCurso == strId
+                        select oCurso
+                    ).FirstOrDefault();
+
+                bdcolegio.CURSO.Remove(objCurso);
+                bdcolegio.SaveChanges();
+
+                return true;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
+        public List<CursoBE> GetCursos()
+        {
+            BDCOLEGIOEntities bdcolegio = new BDCOLEGIOEntities();
+            try
+            {
+                List<CursoBE> objListaCursos = new List<CursoBE>();
+
+                var query = (from oCurso in bdcolegio.CURSO
+                             select oCurso);
+                foreach (var objCurso in query)
+                {
+                    CursoBE objCursoBE = new CursoBE();
+                    objCursoBE.IdCurso = (short)objCurso.IdCurso;
+                    objCursoBE.Descripcion = objCurso.Descripcion;
+
+                    objListaCursos.Add(objCursoBE);
+                }
+
+                return objListaCursos;
+            }
+            catch (EntityException ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+    }
+}
