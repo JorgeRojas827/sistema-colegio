69310597b39578a3713293ea5893c28385f01d81
DeveloperAE <aespinozaanco38@gmail.com>
2022-05-31 15:17:15 -0500

    Anderson2 .0

--- WCF-Colegio/SistemaColegio.Designer.cs
+++ WCF-Colegio/SistemaColegio.Designer.cs
@@ -1,4 +1,4 @@
-﻿// La generación de código T4 está habilitada para el modelo 'D:\git2.0\Sistema_colegio\sistema-colegio\WCF-Colegio\SistemaColegio.edmx'. 
+﻿// La generación de código T4 está habilitada para el modelo 'D:\git2.0\WCF-Colegio\SistemaColegio.edmx'. 
 // Para habilitar la generación de código heredada, cambie el valor de la propiedad del diseñador 'Estrategia de generación de código'
 // por 'ObjectContext heredado'. Esta propiedad está disponible en la ventana Propiedades cuando se abre
 // el modelo en el diseñador.
